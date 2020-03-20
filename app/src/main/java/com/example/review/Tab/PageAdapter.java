00ea34f07800dc157aceecfd2b9e87fa2fdce5c8
TungSuAdr99 <you@example.com>
2020-03-20 22:06:06 +0700

    lam man hinh toi yeu ti

---
+++ app/src/main/java/com/example/review/Tab/PageAdapter.java
@@ -0,0 +1,45 @@
+package com.example.review.Tab;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.review.Fragment.HomeFragment;
+
+public class PageAdapter extends FragmentPagerAdapter {
+    private int numoftabs;
+
+    public PageAdapter(@NonNull FragmentManager fm, int numoftabs) {
+        super(fm);
+        this.numoftabs = numoftabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position)
+        {
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new TabSp();
+            case 2:
+                return new TabDc();
+            case 3:
+                return new TabUd();
+            default:
+                return new TabTkn();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numoftabs;
+    }
+
+    @Override
+    public int getItemPosition(@NonNull Object object) {
+        return POSITION_NONE;
+    }
+}
